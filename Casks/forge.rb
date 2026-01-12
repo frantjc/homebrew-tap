5a789e27f5a358a019f31f244b95054e1280f5ed
Alex Logsdon <39865011+frantjc@users.noreply.github.com>
2026-01-11 23:32:15 -0500

    chore: bump forge to v1.0.5

--- Casks/forge.rb
+++ Casks/forge.rb
@@ -3,27 +3,26 @@ cask "forge" do
   homepage "https://github.com/frantjc/forge"
   version "v1.0.5"
   binary "forge"
-  on_darwin do
-    on_amd64 do
-      url "https://github.com/frantjc/forge/releases/download/v1.0.5/forge-v1.0.5-darwin-amd64.tar.gz"
-      sha256 "41af1a8c681a45d9aa73b90cb8282f69756d56dfcbd39cf41fccfb6fa1584f52"
-    end
-    on_arm64 do
-      url "https://github.com/frantjc/forge/releases/download/v1.0.5/forge-v1.0.5-darwin-arm64.tar.gz"
-      sha256 "9c22ea38e47086a5258b682aea352a4269d2123b1c2fd6e16afb29d9336b471e"
-    end
+
+  livecheck do
+    skip "Auto-generated on release."
   end
   on_linux do
-    on_amd64 do
-      url "https://github.com/frantjc/forge/releases/download/v1.0.5/forge-v1.0.5-linux-amd64.tar.gz"
-      sha256 "91e5fa69f3fc740bf921a9d56a4b410f8d8896f8a4998915791b8414f5d70efe"
-    end
-    on_arm64 do
+    on_arm do
       url "https://github.com/frantjc/forge/releases/download/v1.0.5/forge-v1.0.5-linux-arm64.tar.gz"
-      sha256 "44fdfb46833b66f57513fa0bbd39607517200464ae2dda98e4b49618935d3cba"
+      sha256 "fd47af9369f7bc0d17699e216ac831f893df533df00d8ffa93ab0e1475e08167"
+    end
+    on_intel do
+      url "https://github.com/frantjc/forge/releases/download/v1.0.5/forge-v1.0.5-linux-amd64.tar.gz"
+      sha256 "3d129267c01796b0464314f2444b6c86a15d3925650d4495a6bf528f281d3d22"
     end
   end
-
+  on_macos do
+    on_arm do
+      url "https://github.com/frantjc/forge/releases/download/v1.0.5/forge-v1.0.5-darwin-arm64.tar.gz"
+      sha256 "a51c62c7b29e2fe4bdf194f5741498d436d43d9298f44262a9167ae52f3e259b"
+    end
+  end
   postflight do
     if OS.mac?
       if system_command("/usr/bin/xattr", args: ["-h"]).exit_status == 0
