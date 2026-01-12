ee2809586b4f993e41da760cb9ea78da4f082363
Alex Logsdon <39865011+frantjc@users.noreply.github.com>
2026-01-11 22:21:21 -0500

    chore: bump rvglsm to v0.6.1

--- Casks/rvglsm.rb
+++ Casks/rvglsm.rb
@@ -3,27 +3,26 @@ cask "rvglsm" do
   homepage "https://github.com/frantjc/rvgl-utils"
   version "v0.6.1"
   binary "rvglsm"
-  on_darwin do
-    on_amd64 do
-      url "https://github.com/frantjc/rvgl-utils/releases/download/v0.6.1/rvglsm-v0.6.1-darwin-amd64.tar.gz"
-      sha256 "189a3ac42cd1aff904646c9ebfc176e1e1c59bd79c5e7698d39316dec646e3b8"
-    end
-    on_arm64 do
-      url "https://github.com/frantjc/rvgl-utils/releases/download/v0.6.1/rvglsm-v0.6.1-darwin-arm64.tar.gz"
-      sha256 "9aa3315b2bc3a317b3d850ae7ded1b0958d6003048279cbf5ae6eceefb1630ad"
-    end
+
+  livecheck do
+    skip "Auto-generated on release."
   end
   on_linux do
-    on_amd64 do
-      url "https://github.com/frantjc/rvgl-utils/releases/download/v0.6.1/rvglsm-v0.6.1-linux-amd64.tar.gz"
-      sha256 "0c07c553154b584e4005602091d2a15b78f3805f6c43a5296416921c132fe557"
-    end
-    on_arm64 do
+    on_arm do
       url "https://github.com/frantjc/rvgl-utils/releases/download/v0.6.1/rvglsm-v0.6.1-linux-arm64.tar.gz"
-      sha256 "826a909b5da6146bb040bfa57243e5f3e4514eef2dd1e1ed6da5758fdb27ca1e"
+      sha256 "8cee50c9eaa1761dd05635a88b1741c599e3a673b44aa585cd6a65d7eb9f9ff2"
+    end
+    on_intel do
+      url "https://github.com/frantjc/rvgl-utils/releases/download/v0.6.1/rvglsm-v0.6.1-linux-amd64.tar.gz"
+      sha256 "cc182a8e3610ca12d0d84076b68df60ad6ddc2180f10cccb3a3b807c7ba40c83"
     end
   end
-
+  on_macos do
+    on_arm do
+      url "https://github.com/frantjc/rvgl-utils/releases/download/v0.6.1/rvglsm-v0.6.1-darwin-arm64.tar.gz"
+      sha256 "a856280b4bfec778cdfa4786e35c28d0900c994f186727f274c105fc8255a3bc"
+    end
+  end
   postflight do
     if OS.mac?
       if system_command("/usr/bin/xattr", args: ["-h"]).exit_status == 0
