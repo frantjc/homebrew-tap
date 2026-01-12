b4f64b84baaa3b02802180681dd3d38b9f0f31f0
Alex Logsdon <39865011+frantjc@users.noreply.github.com>
2026-01-11 20:04:25 -0500

    "chore: bump barge to v0.1.2"

--- Casks/barge.rb
+++ Casks/barge.rb
@@ -1,27 +1,26 @@
 cask "barge" do
-  desc "Copy Helm Charts in between archives, releases, repositories, OCI registries, source code, and more.
-"
+  desc "Copy Helm Charts in between archives, releases, repositories, OCI registries, source code, and more."
   homepage "https://github.com/frantjc/barge"
   version "v0.1.2"
   binary "barge"
   on_darwin do
     on_amd64 do
       url "https://github.com/frantjc/barge/releases/download/v0.1.2/barge-v0.1.2-darwin-amd64.tar.gz"
-      sha256 "sha256:6b6160b81b816173cc5e34d70aed21731ff0e56115032ce443693c0f3586d7c7"
+      sha256 "19b83343f5c2243cb0180775ac351ae510d6ed989fd62ad7a09fea851c50b490"
     end
     on_arm64 do
       url "https://github.com/frantjc/barge/releases/download/v0.1.2/barge-v0.1.2-darwin-arm64.tar.gz"
-      sha256 "sha256:e744174a5ef24949d15127b4fdf5171e4d6e8325c8a783213a019efb19dd4927"
+      sha256 "4c269375744e906e5201c36977f322bddc8ab3ab88e5d5536749800a955dd803"
     end
   end
   on_linux do
     on_amd64 do
       url "https://github.com/frantjc/barge/releases/download/v0.1.2/barge-v0.1.2-linux-amd64.tar.gz"
-      sha256 "sha256:2d4b700c0f6d4d1f33fe1ab8108f0f0bdca1997093dfc9b7d7210e2ee5413a15"
+      sha256 "2d97b946dd53f663dd87180f6a8f042770d37f2f87e8f0c7709955aac1c339e4"
     end
     on_arm64 do
       url "https://github.com/frantjc/barge/releases/download/v0.1.2/barge-v0.1.2-linux-arm64.tar.gz"
-      sha256 "sha256:7ab370f9e8c9f1b0481e42c3a513e0ae10440a0fe5db3808b5761d463ae9f2cc"
+      sha256 "127d436725ebf7ceef036a53e78af92e587f9227637c423e548cb0c710da5d08"
     end
   end
 
